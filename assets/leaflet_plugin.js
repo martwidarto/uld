d092cd03f98c2b723f3ee60553507e8b3c3d9785
anshori <m.anshory@gmail.com>
2024-08-04 22:46:23 +0700

    Add legend control, change default control layer to leaflet control layers tree

--- assets/leaflet_plugin.js
+++ assets/leaflet_plugin.js
@@ -7,24 +7,55 @@ map.addControl(L.control.basemaps({
 	tileZ: 1 // tile zoom level
 }));
 
-// Control Layer
-var layers = {
-	"Disabilitas Pasuruan<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/lightgreenmarker.png' width='24'> 1 - 10<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/orangemarker.png' width='24'> 11 - 20<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/redmarker.png' width='24'> > 20": disabilitas_pasuruan,
-	"Disabilitas Kota Kediri<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/lightgreenmarker.png' width='24'> 1 - 10<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/orangemarker.png' width='24'> 11 - 20<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/redmarker.png' width='24'> > 20": disabilitas_kedirikot,
-	"Disabilitas Kabupaten Blitar<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/lightgreenmarker.png' width='24'> 1 - 10<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/orangemarker.png' width='24'> 11 - 20<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/redmarker.png' width='24'> > 20": disabilitas_blitarkab,
-	"Disabilitas Kota Malang<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/lightgreenmarker.png' width='24'> 1 - 10<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/orangemarker.png' width='24'> 11 - 20<br>&nbsp;&nbsp;&nbsp;<img src='assets/images/legend/redmarker.png' width='24'> > 20": disabilitas_malangkot,
-	"Batas Desa Pasuruan": batasdesa_pasuruan,
-	"Batas Kecamatan Pasuruan": bataskecamatan_pasuruan,
-	"Batas Desa Kota Kediri": batasdesa_kedirikot,
-	"Batas Kecamatan Kota Kediri": bataskecamatan_kedirikot,
-	"Batas Desa Kabupaten Blitar": batasdesa_blitarkab,
-	"Batas Kecamatan Kabupaten Blitar": bataskecamatan_blitarkab,
-	"Batas Desa Kota Malang": batasdesa_malangkot,
-	"Batas Kecamatan Kota Malang": bataskecamatan_malangkot,
-	"Multi Bahaya<br>&nbsp;&nbsp;&nbsp;&nbsp;<img src='assets/images/indeks_bahaya_kerentanan_risiko.png' width='85' height='15'><br>&nbsp;&nbsp;&nbsp;&nbsp;<small>Rendah&nbsp;&nbsp;&nbsp;&nbsp;Tinggi</small>": layer_MultiBahaya
-};
+var layers = [
+	{
+		label: ' <b>Disabilitas</b>',
+		selectAllCheckbox: true,
+		children: [
+			{ label: ' Pasuruan', layer: disabilitas_pasuruan},
+			{ label: ' Kota Kediri', layer: disabilitas_kedirikot},
+			{ label: ' Kota Malang', layer: disabilitas_malangkot},
+			{ label: ' Kabupaten Blitar', layer: disabilitas_blitarkab},
+		]
+	},
+	{
+		label: ' <b>Batas Kecamatan</b>',
+		selectAllCheckbox: true,
+		collapsed: true,
+		children: [
+			{ label: ' Pasuruan', layer: bataskecamatan_pasuruan },
+			{ label: ' Kota Kediri', layer: bataskecamatan_kedirikot },
+			{ label: ' Kota Malang', layer: bataskecamatan_malangkot },
+			{ label: ' Kabupaten Blitar', layer: bataskecamatan_blitarkab },
+		]
+	},
+	{
+		label: ' <b>Batas Desa</b>',
+		selectAllCheckbox: true,
+		collapsed: true,
+		children: [
+			{ label: ' Pasuruan', layer: batasdesa_pasuruan },
+			{ label: ' Kota Kediri', layer: batasdesa_kedirikot },
+			{ label: ' Kota Malang', layer: batasdesa_malangkot },
+			{ label: ' Kabupaten Blitar', layer: batasdesa_blitarkab },
+		]
+	},
+	{
+		label: ' <b>Bencana</b>',
+		selectAllCheckbox: true,
+		collapsed: true,
+		children: [
+			{ label: ' Multi Bahaya', layer: layer_MultiBahaya },
+		]
+	}
+];
 
-L.control.layers(null, layers, { collapsed: false }).addTo(map);
+L.control.layers.tree(null, layers, {
+	namedToggle: true,
+	collapseAll: '<b><i class="fa-solid fa-folder-closed"></i> Tutup semua</b>',
+	expandAll: '<b><i class="fa-solid fa-folder-open"></i> Buka semua</b>',
+	collapsed: false,
+}).addTo(map);
 
 
 // Scale Bar
@@ -49,3 +80,20 @@ L.control.watermark = function (opts) {
 	return new L.Control.Watermark(opts);
 }
 L.control.watermark({ position: 'bottomleft' }).addTo(map);
+
+// Control legend
+var legend = L.control({
+	position: 'bottomleft'
+});
+
+legend.onAdd = function (map) {
+	this._div = L.DomUtil.create('div', 'legend');
+	this.update();
+	return this._div;
+};
+
+legend.update = function () {
+	this._div.innerHTML = `<div class='p-1 mb-1'><strong>Jumlah Disabilitas</strong><br><img src='assets/images/legend/lightgreenmarker.png' width='24'> 1 - 10<br><img src='assets/images/legend/orangemarker.png' width='24'> 11 - 20<br><img src='assets/images/legend/redmarker.png' width='24'> > 20</div><div class='p-1 mb-1'><strong>Batas Wilayah</strong><br><svg height="4" width="32"><line x1="0" y1="4" x2="32" y2="4" style="stroke:black;stroke-width:12" /></svg> Batas Kecamatan<br><svg height="3" width="32"><line x1="0" y1="3" x2="32" y2="3" style="stroke:gray;stroke-width:12" /></svg> Batas Desa</div><div class='p-1 mb-1'><strong>Multi Bahaya</strong><br><img src='assets/images/indeks_bahaya_kerentanan_risiko.png' width='100' height='15'><br><small>Rendah&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tinggi</small></div>`;
+};
+
+legend.addTo(map);
